9ebc7524c774fddfd030c730f1dbc60713456f68
Kamirus <listopadkamil@gmail.com>
2019-01-23 16:16:13 +0100

    church lists encoding

--- src/ChurchEncodings.purs
+++ src/ChurchEncodings.purs
@@ -2,6 +2,7 @@ module ChurchEncodings where
 
 import Prelude
 
+import Data.Array (reverse, (:))
 import Effect (Effect)
 import Effect.Console (logShow)
 
@@ -62,9 +63,40 @@ isZero n = unNat n (const false_) true_
 pred ∷ Nat → Nat
 pred n = Nat \s z → fst_ $ unNat n (\p → pair (snd_ p) (s $ snd_ p)) (pair z z)
 
+type LIST a = ∀ r. r → (a → r → r) → r
+newtype List a = List (LIST a)
+unList ∷ ∀ a. List a → LIST a
+unList (List x) = x
+
+nil ∷ ∀ a. List a
+nil = List \e c → e
+
+cons ∷ ∀ a. a → List a → List a
+cons x l = List \e c → c x $ unList l e c
+
+map ∷ ∀ a b. (a → b) → List a → List b
+map f l = unList l nil \a acc → cons (f a) acc
+
+len ∷ ∀ a. List a → Int
+len l = unList l 0 \_ acc → acc + 1
+
+toArray ∷ ∀ a. List a → Array a
+toArray l = reverse $ unList l [] \a acc → a : acc
+
 main ∷ Effect Unit
 main = do
   logShow $ if_ true_ 1 0 == 1
   logShow $ if_ false_ 1 0 == 0
   logShow $ fst_ (pair 1 0) == 1
   logShow $ snd_ (pair 1 0) == 0
+
+  let
+    l0 = cons 0 nil
+    l01 = cons 1 l0
+    l012 = cons 2 l01
+  logShow $ toArray (nil ∷ List Int)
+  logShow $ toArray l0
+  logShow $ toArray l01
+  logShow $ toArray l012
+  logShow $ toArray $ map (_+1) l012
+  logShow $ len l012
