534c834c30fecfb3f489396abe4b4c7551467c93
Kamirus <listopadkamil@gmail.com>
2019-12-22 16:19:38 +0100

    exists example - eval fixed

--- src/Exists/Example.purs
+++ src/Exists/Example.purs
@@ -2,8 +2,8 @@ module Exists.Example where
 
 import Prelude
 
-import Data.Leibniz (type (~), coerce, coerceSymm)
-import Data.Tuple (Tuple(..), snd)
+import Data.Foldable (for_)
+import Data.Leibniz (type (~), coerce)
 import Effect (Effect)
 import Effect.Console (log)
 import Exists (Exists, mkExists, runExists)
@@ -16,48 +16,40 @@ data BinOp i o
   = Eq (Boolean ~ o)
   | And (Boolean ~ i) (Boolean ~ o)
 
--- | type for expresions, expr can be just a literal of Int or Boolean
--- | or expresion of binary operator with two expresions.
--- | Notice that `BinOp i o` has two type variables and we have just information about output type `o`.
--- | But we don't care about `i` type here, we just have to know that input expresions have type `Expr i`
--- | So we enclose this information in Exists
+-- | `Expr o` represents an expresion of type `o`
+-- | Notice that `BinOp i o` has two type variables
+-- | type parameter `i` should be existentially quantified
 data Expr o
   = LitInt (Int ~ o) Int
   | LitBoolean (Boolean ~ o) Boolean
-  -- | exists i. BinExpr (BinOp i o) (Expr i) (Expr i) 
+  -- exists i. BinExpr (BinOp i o) (Expr i) (Expr i) 
   | BinExpr (Exists (BinExpr' o))
 
--- | We introduce new type for BinExpr with two type variables `o` and `i`. Notice that they are flipped here.
-data BinExpr' o i = BinExpr' (BinOp i o) (Expr i) (Expr i)
+-- | We introduce new type for `BinExpr` with two type variables `o` and `i`.
+-- | Notice that they are flipped here to hide/pack `i` in `Exists`
+data BinExpr' o i = BinExpr' (BinOp i o) (i → i → o) (Expr i) (Expr i)
 
 main ∷ Effect Unit
-main = log $ show $ map showExpr expressions
+main = do
+  for_ expressions \e →
+    log $ "eval ( " <> showExpr e <> " ) == " <> (show $ eval e)
 
 expressions ∷ Array (Expr Boolean)
 expressions = 
   [ bool true
-  , int 1 `eq` int 2
-  , bool true `and` (int 1 `eq` int 1)
+  , int 1 `eq_` int 2
+  , bool true `and_` (int 1 `eq_` int 1)
   ]
 
--- | failed to implement eval, evalBinExpr cannot implement equality, we lost `Eq a` in exists
--- | maybe new `data Exists' f = Exists' (∀ r. (∀ a. Eq a ⇒ f a → r) → r)` would fix this issue
--- | We implement simple show for expressions. 
+eval ∷ ∀ o. Expr o → o
+eval = case _ of
+  LitInt f x → coerce f x
+  LitBoolean f x → coerce f x
+  BinExpr e → runExists evalBinExpr e
 
--- | eval ∷ ∀ o. Expr o → o
--- | eval = case _ of
--- |   LitInt f x → coerce f x
--- |   LitBoolean f x → coerce f x
--- |   BinExpr e → runExists evalBinExpr e
--- |
--- | evalBinExpr ∷ ∀ o i. BinExpr' o i → o
--- | evalBinExpr (BinExpr' op e1 e2) =
--- |   case op of
--- |     Eq f → coerce f $ eval e1 == eval e2
--- |     And f1 f2 → 
--- |       let (r1 ∷ Boolean) = coerceSymm f1 $ eval e1 in
--- |       let (r2 ∷ Boolean) = coerceSymm f1 $ eval e2 in
--- |       coerce f2 $ r1 && r2
+evalBinExpr ∷ ∀ o i. BinExpr' o i → o
+evalBinExpr (BinExpr' _ f e1 e2) =
+  f (eval e1) (eval e2)
 
 showExpr ∷ ∀ o. Expr o → String
 showExpr = case _ of
@@ -66,7 +58,7 @@ showExpr = case _ of
   BinExpr e → runExists showBinExpr e
 
 showBinExpr ∷ ∀ o i. BinExpr' o i → String
-showBinExpr (BinExpr' op e1 e2) =
+showBinExpr (BinExpr' op _ e1 e2) =
   case op of
     Eq _ → "(" <> showExpr e1 <> " == " <> showExpr e2 <> ")"
     And _ _ → "(" <> showExpr e1 <> " && " <> showExpr e2 <> ")"
@@ -77,23 +69,8 @@ int = LitInt identity
 bool ∷ Boolean → Expr Boolean
 bool = LitBoolean identity
 
-and ∷ Expr Boolean → Expr Boolean → Expr Boolean
-and e1 e2 = BinExpr $ mkExists (BinExpr' (And identity identity) e1 e2)
+and_ ∷ Expr Boolean → Expr Boolean → Expr Boolean
+and_ e1 e2 = BinExpr $ mkExists (BinExpr' (And identity identity) (&&) e1 e2)
 
-eq ∷ ∀ i. Eq i ⇒ Expr i → Expr i → Expr Boolean
-eq e1 e2 = BinExpr $ mkExists (BinExpr' (Eq identity) e1 e2)
-
--- | ------------------------------------------------------
-
--- | example from https://github.com/purescript/purescript-exists/blob/v4.0.0/src/Data/Exists.purs#L27-L27
-data StreamF a s = StreamF s (s -> Tuple s a)
-type Stream a = Exists (StreamF a)
-
-nats ∷ Stream Int
-nats = mkExists $ StreamF 0 (\n → Tuple (n + 1) n)
-
-head ∷ ∀ a. Stream a → a
-head = runExists head'
-  where
-  head' ∷ ∀ s. StreamF a s → a
-  head' (StreamF s f) = snd (f s)
+eq_ ∷ ∀ i. Eq i ⇒ Expr i → Expr i → Expr Boolean
+eq_ e1 e2 = BinExpr $ mkExists (BinExpr' (Eq identity) eq e1 e2)
