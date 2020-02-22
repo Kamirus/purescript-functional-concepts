21d9f595b16a4e8146f153e8eaa6605068c103b7
Kamirus <listopadkamil@gmail.com>
2020-02-22 22:16:12 +0100

    add more existential schemes

--- src/Exists.purs
+++ src/Exists.purs
@@ -4,6 +4,8 @@ module Exists
   , runExists
   ) where
 
+import Unsafe.Coerce (unsafeCoerce)
+
 -- | https://wiki.haskell.org/Rank-N_types
 -- | data Exists f = forall a. Exists (f a)
 data Exists f = Exists (∀ r. (∀ a. f a → r) → r)
@@ -13,3 +15,38 @@ mkExists fa = Exists \g → g fa
 
 runExists ∷ ∀ f r. (∀ a. f a → r) → Exists f → r
 runExists g (Exists un) = un g
+
+
+-- existential pattern
+-- credits: natefaubion
+
+foreign import data Exists2 ∷ (Type → Type → Type) → Type
+
+mkExists2 ∷ ∀ f a b. f a b → Exists2 f
+mkExists2 = unsafeCoerce
+
+runExists2 ∷ ∀ f r. (∀ a b. f a b → r) → Exists2 f → r
+runExists2 = unsafeCoerce
+
+
+-- unsafe existential - can help retain stack safety
+-- credits: Denis Stoyanov, natefaubion
+
+-- data Ap f a where
+--   Pure ∷ a → Ap f a
+--   Ap   ∷ exists b. f a → Ap f (a → b) → Ap f b
+
+data E
+
+data FreeAp f a
+  = Pure (a → FreeAp f a)
+  | Ap (FreeAp f (E → a)) (FreeAp f E)
+
+ap ∷ ∀ f a b. FreeAp f (b → a) → FreeAp f b → FreeAp f a
+ap fba fb = Ap (coerceFunc fba) (coerceValue fb)
+  where
+    coerceFunc ∷ FreeAp f (b → a) → FreeAp f (E → a)
+    coerceFunc = unsafeCoerce
+    
+    coerceValue ∷ FreeAp f b → FreeAp f E
+    coerceValue = unsafeCoerce
